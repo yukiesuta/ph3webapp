b2490d0085b7a55b2ada922c8f5b9dada4b8c46f
yukiesuta <matsumoto.yukiho.0704@gmail.com>
2022-09-30 23:12:03 +0900

    improve seeder file

--- src/database/seeds/DataTableSeeder.php
+++ src/database/seeds/DataTableSeeder.php
@@ -15,10 +15,58 @@ class DataTableSeeder extends Seeder
             [
                 'user_id' => 1,
                 'date' => '2022-03-01',
+                'language_id' => 2,
+                'content_id' => 1,
+                'hour' => 3
+            ],[
+                'user_id' => 1,
+                'date' => '2022-03-02',
+                'language_id' => 1,
+                'content_id' => 2,
+                'hour' => 1
+            ],[
+                'user_id' => 1,
+                'date' => '2022-03-03',
+                'language_id' => 2,
+                'content_id' => 1,
+                'hour' => 1
+            ],[
+                'user_id' => 1,
+                'date' => '2022-03-04',
+                'language_id' => 3,
+                'content_id' => 2,
+                'hour' => 8
+            ],[
+                'user_id' => 2,
+                'date' => '2022-03-02',
+                'language_id' => 2,
+                'content_id' => 1,
+                'hour' => 5
+            ],[
+                'user_id' => 2,
+                'date' => '2022-03-04',
                 'language_id' => 1,
                 'content_id' => 1,
                 'hour' => 1
-            ]
+            ],[
+                'user_id' => 2,
+                'date' => '2022-03-05',
+                'language_id' => 1,
+                'content_id' => 1,
+                'hour' => 1
+            ],[
+                'user_id' => 1,
+                'date' => '2022-03-01',
+                'language_id' => 1,
+                'content_id' => 1,
+                'hour' => 1
+            ],[
+                'user_id' => 1,
+                'date' => '2022-03-01',
+                'language_id' => 1,
+                'content_id' => 1,
+                'hour' => 1
+            ],
         ];
         DB::table('data')->insert($params);
     }
