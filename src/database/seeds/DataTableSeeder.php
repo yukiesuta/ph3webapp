57a80eb16d55a71fe79592086a3c19a4866fbf1b
yukiesuta <matsumoto.yukiho.0704@gmail.com>
2022-10-06 01:05:37 +0900

    add seeder file

--- src/database/seeds/DataTableSeeder.php
+++ src/database/seeds/DataTableSeeder.php
@@ -51,21 +51,81 @@ class DataTableSeeder extends Seeder
             ],[
                 'user_id' => 2,
                 'date' => '2022-03-05',
-                'language_id' => 1,
+                'language_id' => 6,
                 'content_id' => 1,
-                'hour' => 1
+                'hour' => 5
             ],[
                 'user_id' => 1,
                 'date' => '2022-03-01',
-                'language_id' => 1,
+                'language_id' => 5,
+                'content_id' => 2,
+                'hour' => 3
+            ],[
+                'user_id' => 2,
+                'date' => '2022-03-20',
+                'language_id' => 7,
                 'content_id' => 1,
                 'hour' => 1
             ],[
+                'user_id' => 2,
+                'date' => '2022-03-16',
+                'language_id' => 7,
+                'content_id' => 2,
+                'hour' => 6
+            ],[
+                'user_id' => 1,
+                'date' => '2022-03-15',
+                'language_id' => 5,
+                'content_id' => 1,
+                'hour' => 3
+            ],[
+                'user_id' => 1,
+                'date' => '2022-03-13',
+                'language_id' => 4,
+                'content_id' => 2,
+                'hour' => 7
+            ],[
+                'user_id' => 1,
+                'date' => '2022-03-11',
+                'language_id' => 1,
+                'content_id' => 4,
+                'hour' => 9
+            ],[
                 'user_id' => 1,
                 'date' => '2022-03-01',
-                'language_id' => 1,
+                'language_id' => 3,
                 'content_id' => 1,
+                'hour' => 7
+            ],[
+                'user_id' => 2,
+                'date' => '2022-03-09',
+                'language_id' => 7,
+                'content_id' => 1,
+                'hour' => 2
+            ],[
+                'user_id' => 1,
+                'date' => '2022-03-25',
+                'language_id' => 4,
+                'content_id' => 1,
+                'hour' => 5
+            ],[
+                'user_id' => 2,
+                'date' => '2022-03-21',
+                'language_id' => 2,
+                'content_id' => 2,
                 'hour' => 1
+            ],[
+                'user_id' => 1,
+                'date' => '2022-03-25',
+                'language_id' => 6,
+                'content_id' => 3,
+                'hour' => 29
+            ],[
+                'user_id' => 2,
+                'date' => '2022-03-28',
+                'language_id' => 8,
+                'content_id' => 1,
+                'hour' => 5
             ],
         ];
         DB::table('data')->insert($params);
