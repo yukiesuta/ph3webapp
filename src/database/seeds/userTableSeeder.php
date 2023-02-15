ceb6877ecf70d7c2ea3e03cf15bd3bfac782a8e3
yukiho <matsumoto.yukiho.0704@gmail.com>
2023-02-15 23:05:49 +0900

    miner fix

--- src/database/seeds/userTableSeeder.php
+++ src/database/seeds/userTableSeeder.php
@@ -12,9 +12,9 @@ class userTableSeeder extends Seeder
     public function run()
     {
         $param = [
-            ['name'=>'武田龍一','email'=>'user0@posse.com','password'=>Hash::make('password')],
-            ['name'=>'石川朝香','email'=>'user1@posse.com','password'=>Hash::make('password')],
-            ['name'=>'松本透歩','email'=>'user2@posse.com','password'=>Hash::make('password')],
+            ['name'=>'user0','email'=>'user0@posse.com','password'=>Hash::make('password')],
+            ['name'=>'user1','email'=>'user1@posse.com','password'=>Hash::make('password')],
+            ['name'=>'user2','email'=>'user2@posse.com','password'=>Hash::make('password')],
         ];
 
         DB::table('users')->insert($param);
